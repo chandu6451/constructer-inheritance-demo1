31934ab510300eddf8b4aece8766bea7225fd308
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:38:50 +0530

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorInheritanceDemo1
+{
+    public class Manager : Employee
+    {
+        public Manager()
+        {
+            Console.WriteLine("Manager Class Default Constructor");
+        }
+
+        public Manager(int id, string name, double sal, double comp)
+        {
+            Console.WriteLine("Manager Class Parameterized Constructor");
+        }
+    }
+}
